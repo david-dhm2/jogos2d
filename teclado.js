5bf59926d9cf8bb1dca4397c07837484cf7f20b3
david hume <canaldodavidhume@gmail.com>
2023-07-06 19:36:14 -0300

    uma forma mais fluida de se mover

---
+++ teclado.js
@@ -0,0 +1,40 @@
+//isto e a aba de funções do teclado
+
+//this e um focalizador no mome horiginal do atributo
+
+//var simplesmente cria uma variavel... dicionario existe
+
+//true se a ação e verdadeira
+
+//false se a ação e falsa
+
+//function cria ou coloca uma funsão ou emplementa exp function(tecla)
+
+//else if e tipo um "filosofo" exp e se tal coisa acontece
+
+//return define o valor retornado por uma função. Quando não há um valor-
+//especificado, undefined será retornado.
+//Interrompe a execução da função atual.
+
+var SETA_ESQUERDA = 37;
+var SETA_DIREITA = 39;
+
+function Teclado (elemento) {
+		this.elemento = elemento;
+		//array(al) de teclas precionadas
+		this.pressionadas = [];
+		
+		//registro de estado de tecla no 4rra7,7431
+		var teclado = this;
+		elemento.addEventListener('Keydown', function(evento){
+				teclado.pressionadas[evento.keyCode] = true;
+		});
+		elemento.addEventListener('Keyup', function(evento){
+				teclado.pressionadas[evento.keyCode] = false;
+		});
+	}
+	Teclado.prototype = {
+		pressionada: function(tecla) {
+			return this.pressionadas[tecla];
+		}
+	}
